69dedf8440bc61dcf15fa7921335946d0df6f1a4
Jtaylor26 <jtaylor26@sheffield.ac.uk>
2018-11-27 21:14:09 +0000

    Update RegistrarFunctions.java
    Changed VerifyCreditTotal to return the total credits. Check now done in new function listOptionalModules that returns a list of modules that can be added.

--- RegistrarFunctions.java
+++ RegistrarFunctions.java
@@ -2,6 +2,7 @@
  * RegistrarFunctions.java
  * @author Matt Prestwich
  * @author Byron Slater
+ * @author James Taylor
  */
 
 /**
@@ -11,6 +12,7 @@
 package systemsProject;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class RegistrarFunctions {
 	/**
@@ -95,18 +97,44 @@ public class RegistrarFunctions {
 		}
 	}
 	
-	/*/** WIP
-	 * Function employed to add optional modules on behalf of students.
+
+	/**
+	 * Function employed to list optional modules for a student. An empty list indicates full credits.
 	 * @throws SQLException
-	 *
-	public static void addOptionalModule() throws SQLException {
+	 */
+	public static ArrayList<String> listOptionalModule(String StudentID) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ArrayList<String> optionalModules = new ArrayList<String>();
 		try {
-			con = SQLFunctions.connectToDatabase();			
+			con = SQLFunctions.connectToDatabase();	
 			pstmt = con.prepareStatement(
-					"SELECT * FROM DegreeModule WHERE DegreeLevel = ? AND Core = N");
-			pstmt.setString(1, PeriodID + StudentID);
-			pstmt.executeUpdate();
-			System.out.println("Module added successfully.");
+					"SELECT * MAX(PeriodID) FROM StudentPeriod WHERE StudentID = ?");
+			pstmt.setString(1, StudentID);
+			ResultSet period = pstmt.executeQuery();
+			period.next();
+			String StudentPeriod = period.getString(1);
+			String DegreeLevel = period.getString(3);
+			period.close();
+			int creditTotal = verifyCreditTotal(StudentPeriod);
+			int remainingCredits = 0;
+			
+			if (DegreeLevel.charAt(4) == 'U') 
+				remainingCredits = 120 - creditTotal;
+			else if (DegreeLevel.charAt(4) == 'P') 
+				remainingCredits = 180 - creditTotal;	
+			if (remainingCredits > 0) {
+				pstmt = con.prepareStatement(
+						"SELECT * FROM DegreeModule WHERE DegreeLevel = ? AND CORE = 0");
+				pstmt.setString(1, DegreeLevel);
+				ResultSet modules = pstmt.executeQuery();
+				while (modules.next()) {
+					if (modules.getInt(2) > remainingCredits)
+						optionalModules.add(modules.getString(1));
+				}
+				modules.close();
+			}
+            return optionalModules;
 		}
 		catch (SQLException ex) {
 			SQLFunctions.defaultError(ex, con, stmt, pstmt);
@@ -114,12 +142,12 @@ public class RegistrarFunctions {
 		finally {
 			SQLFunctions.closeAll(con, stmt, pstmt);
 		}
-	}*/
+	}
 	
 	/**
-	 * Function employed to verify credit totals.
+	 * Function employed to return the total number of credits for a students current degree course.
 	 */
-	public static void verifyCreditTotal(String StudentID) throws SQLException {
+	public static int verifyCreditTotal(String StudentPeriod) throws SQLException {
 		int creditTotal = 0;
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -127,40 +155,30 @@ public class RegistrarFunctions {
 		try {
 			con = SQLFunctions.connectToDatabase();			
 			pstmt = con.prepareStatement(
-					"SELECT * FROM StudentPeriod WHERE StudentID = ?");
-			pstmt.setString(1, StudentID);
+					"SELECT DegreeLevel FROM StudentPeriod WHERE StudentPeriod = ?");
+			pstmt.setString(1, StudentPeriod);
 			ResultSet student = pstmt.executeQuery();
 			student.next();
-			String DegreeLevel = student.getString(3);
+			String DegreeLevel = student.getString(1);
 			student.close();
 
 			pstmt = con.prepareStatement(
-					"SELECT * FROM DegreeModule WHERE DegreeLevel = ? AND CORE = 1");
+					"SELECT * FROM DegreeModule WHERE DegreeLevel = ?");
 			pstmt.setString(1, DegreeLevel);
 			ResultSet modules = pstmt.executeQuery();
 			
 			while (modules.next()) {
 				pstmt = con.prepareStatement(
-						"SELECT * FROM Module WHERE ModuleID = ?");
+						"SELECT ModuleID,Credits FROM Module WHERE ModuleID = ?");
 				pstmt.setString(1, modules.getString(1));
 				ResultSet module = pstmt.executeQuery();
 				module.next();
-				creditTotal += module.getInt(3);
+				creditTotal += module.getInt(2);
 				module.close();
 			}
 			modules.close();
-			
-			if (DegreeLevel.charAt(4) == 'U') {
-				if (creditTotal == 120)
-					System.out.println("Student fully registered.");
-				else
-					System.out.println("Student must take 120 credits of modules.");
-			} else if (DegreeLevel.charAt(4) == 'P') {
-				if (creditTotal == 180)
-					System.out.println("Student fully registered");
-				else
-					System.out.println("Student must take 180 credits of modules.");
-			}
+			return creditTotal;
+
 		}
 		catch (SQLException ex) {
 		    ex.printStackTrace();
